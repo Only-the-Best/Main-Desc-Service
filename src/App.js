fc8ee96dc14729f91e9b2085985d7fd431d0e7a4
Rocky Durbin <rockydurbin8@gmail.com>
2018-11-10 18:19:09 -0500

    Added css and skeleton

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+// import "./App.css";
+import MainDescription from "./components/MainDescription"
+
+class App extends Component{
+  render(){
+    return(
+      <MainDescription />
+    );
+  }
+}
+
+export default App;
---
+++ src/components/MainDescription.js
@@ -0,0 +1,193 @@
+import React, { Component} from "react";
+// import "MainDescription.css";
+
+class MainDescription extends Component{
+  render(){
+    return(
+<div id="DIV_1">
+  <div id="DIV_2">
+    <div id="DIV_3">
+      <div id="DIV_4">
+        <header id="HEADER_6">
+          <h1 id="H1_7">
+            <div id="DIV_8">
+              6164 Wilding Rd
+            </div>
+            <div id="DIV_9">
+              Ravenswood, WV 26164
+            </div>
+          </h1>
+          <h3 id="H3_10">
+            <span id="SPAN_11"></span> <span id="SPAN_12">3 beds</span><span id="SPAN_13"></span> <span id="SPAN_14">3 baths</span><span id="SPAN_15"></span> <span id="SPAN_16">4,150 sqft</span>
+          </h3>
+        </header>
+      </div>
+    </div>
+    <div id="DIV_18">
+      <div id="DIV_21">
+        <div id="DIV_22">
+          <span id="SPAN_23"></span> For Sale
+        </div>
+        <div id="DIV_24">
+          <span id="SPAN_25">$579,000</span>
+        </div>
+        <div id="DIV_26">
+          <a id="A_27">View Zestimate</a>
+        </div>
+      </div>
+      <div id="DIV_28">
+          <h4 id="H4_30">
+          </h4><span id="SPAN_31"><span id="SPAN_32">Est. Mortgage:</span> $2,397/mo</span>
+      </div>
+    </div>
+  </div>
+  <div id="DIV_104">
+    <div id="DIV_105">
+      <div id="DIV_106">
+        <div id="DIV_107">
+          Gorgeous Newly Remodeled Home Sitting In A Private Country Setting. A Hunter's Paradise Offering Nearly 119 Acres With Perfect Ground For Trail Riding. Property Features A 60x120 Pole Building And A Separate Garage Apartment. This Beautiful Home Features
+          An Spacious Open Floor Plan With 3 Br/2.5 Baths. Brand New Hardwood Floors Throughout. New Kitchen Counter Tops, Appliances And A New Stone Fireplace In The Living Room. Enjoy A Huge Back Porch For All Your Entertaining Needs! A Must See!
+        </div>
+      </div>
+      <div id="DIV_108">
+        <h3 id="H3_109">
+          What I Love About The Home
+        </h3>
+        <div id="DIV_110">
+          Place to hide. Not be bothered with the hussle and bussle of everyday stress of life. With all the modern amenities. Come find your new house.
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="DIV_111">
+    <div id="DIV_112">
+      <p id="P_1">
+        Facts and Features
+      </p>
+      <div id="DIV_113">
+        <div id="DIV_114">
+          <div id="DIV_115">
+            <span id="SPAN_4"></span>
+          </div>
+          <div id="DIV_116">
+            <div id="DIV_117">
+              Type
+            </div>
+            <div id="DIV_118">
+              Single Family
+            </div>
+          </div>
+        </div>
+        <div id="DIV_119">
+          <div id="DIV_120">
+            <span id="SPAN_10"></span>
+          </div>
+          <div id="DIV_121">
+            <div id="DIV_122">
+              Year Built
+            </div>
+            <div id="DIV_123">
+              2000
+            </div>
+          </div>
+        </div>
+        <div id="DIV_124">
+          <div id="DIV_125">
+            <span id="SPAN_27"></span>
+          </div>
+          <div id="DIV_126">
+            <div id="DIV_127">
+              Heating
+            </div>
+            <div id="DIV_128">
+              Heat pump
+            </div>
+          </div>
+        </div>
+        <div id="DIV_129">
+          <div id="DIV_130">
+            <span id="SPAN_22"></span>
+          </div>
+          <div id="DIV_131">
+            <div id="DIV_132">
+              Cooling
+            </div>
+            <div id="DIV_133">
+              Central
+            </div>
+          </div>
+        </div>
+        <div id="DIV_134">
+          <div id="DIV_135">
+            <span id="SPAN_28"></span>
+          </div>
+          <div id="DIV_136">
+            <div id="DIV_137">
+              Parking
+            </div>
+            <div id="DIV_138">
+              5 spaces
+            </div>
+          </div>
+        </div>
+        <div id="DIV_139">
+          <div id="DIV_140">
+            <span id="SPAN_34"></span>
+          </div>
+          <div id="DIV_141">
+            <div id="DIV_142">
+              Lot
+            </div>
+            <div id="DIV_143">
+              119 acres
+            </div>
+          </div>
+        </div>
+        <div id="DIV_144">
+          <div id="DIV_145">
+            <span id="SPAN_40"></span>
+          </div>
+          <div id="DIV_146">
+            <div id="DIV_147">
+              Days on Zillow
+            </div>
+            <div id="DIV_148">
+              49 Days
+            </div>
+          </div>
+        </div>
+        <div id="DIV_149">
+          <div id="DIV_150">
+            <span id="SPAN_46"></span>
+          </div>
+          <div id="DIV_151">
+            <div id="DIV_152">
+              Price/sqft
+            </div>
+            <div id="DIV_153">
+              $140
+            </div>
+          </div>
+        </div>
+        <div id="DIV_154">
+          <div id="DIV_155">
+            <span id="SPAN_52"></span>
+          </div>
+          <div id="DIV_156">
+            <div id="DIV_157">
+              Saves
+            </div>
+            <div id="DIV_158">
+              3
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+    );
+  }
+}
+
+export default MainDescription;
